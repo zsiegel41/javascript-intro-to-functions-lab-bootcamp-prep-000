85a41e7633b1364e152c9050985ccfc0a96345c9
Zach Siegel <zachsiegel41@gmail.com>
2017-09-10 03:10:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,18 +7,18 @@ function whisper(string) {
 }
 
 function logShout(string) {
-  console.log(string.toUpperCase)
+  console.log(string.toUpperCase())
 }
 
 function logWhisper(string) {
-  console.log(string.toLowerCase)
+  console.log(string.toLowerCase()
 }
 
 function sayHiToGrandma(string) {
-if (string.toLowerCase === string) {
+if (string.toLowerCase() === string) {
   return "I can't hear you!";
 }
-  else if (string.toUpperCase === string) {
+  else if (string.toUpperCase() === string) {
   return "YES INDEED!";
 }
   else if (string === "I love you, Grandma."){
