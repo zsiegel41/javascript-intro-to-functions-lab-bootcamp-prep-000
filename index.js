c7d48fe5eb601c996554e6c693d8b5c16431a43e
Zach Siegel <zachsiegel41@gmail.com>
2017-09-10 02:53:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ function logWhisper(string) {
   console.log(string.toLowerCase)
 }
 function sayHiToGrandma(string) {
-if string.toLowerCase === string {
+if (string.toLowerCase === string) {
   return "I can't hear you!"}
-  else if string.toUpperCase === string {
+  else if (string.toUpperCase === string) {
   return "YES INDEED!"}
-  else string = "I love you, Grandma." {
+  else (string = "I love you, Grandma.") {
     return "I love you, too."}
 }
