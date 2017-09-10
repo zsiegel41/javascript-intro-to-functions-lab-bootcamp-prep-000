bf3b58e446c07c07499f347a4db022b86d12803b
Zach Siegel <zachsiegel41@gmail.com>
2017-09-10 02:59:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ if (string.toLowerCase === string) {
   else if (string.toUpperCase === string) {
   return "YES INDEED!";
 }
-  else (string = "I love you, Grandma.");{
-    return "I love you, too."
+  else (string = "I love you, Grandma."){
+    return "I love you, too.";
   }
 }
